5560494f156814261b82ae68bd516cba45898907
arond <softincuz@gmail.com>
2024-07-15 12:34:50 +0500

    init

---
+++ src/main/java/sendiy/uz/SendiyApplication.java
@@ -0,0 +1,13 @@
+package sendiy.uz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendiyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SendiyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sendiy/uz/SendiyApplicationTests.java
@@ -0,0 +1,13 @@
+package sendiy.uz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendiyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
